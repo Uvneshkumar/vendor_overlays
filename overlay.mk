56e75c0ada7a7f2b6c96d11a2859071ed6cfe8f4
Uvneshkumar <uvneshkumar@gmail.com>
2021-04-26 04:31:25 +0530

    Launcher3

--- overlay.mk
+++ overlay.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote8pro \
 	treble-overlay-xiaomi-redmis2 \
 	treble-overlay-xiaomi-sweet \
+	treble-overlay-treuchet \
 
 
 PRODUCT_PACKAGES += \
