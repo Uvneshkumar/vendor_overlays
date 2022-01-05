4f5f3a76e13040595bda35e98f8fbec4a2591829
Uvneshkumar <uvneshkumar@gmail.com>
2022-01-06 04:01:55 +0530

    S21 Fixes

--- build/build_and_install.sh
+++ build/build_and_install.sh
@@ -1,4 +1,5 @@
-adb connect 192.168.1.18
+adb connect 192.168.1.16
 ./build.sh
 #adb install -r treble-overlay-xiaomi-sweet.apk
+adb install -r treble-overlay-samsung-s20-one-ui.apk
 adb install -r treble-overlay-samsung-s20-nofod.apk
