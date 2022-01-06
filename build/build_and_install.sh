a8085a825d3ee58d3e6013fc3b2a7f4216b6b877
Uvneshkumar <uvneshkumar@gmail.com>
2022-01-06 21:39:35 +0530

    S21 isStatic overlays \o/

--- build/build_and_install.sh
+++ build/build_and_install.sh
@@ -1,5 +1,9 @@
 adb connect 192.168.1.16
 ./build.sh
 #adb install -r treble-overlay-xiaomi-sweet.apk
-adb install -r treble-overlay-samsung-s20-one-ui.apk
+#adb install -r treble-overlay-samsung-s20-one-ui.apk
 adb install -r treble-overlay-samsung-s20-nofod.apk
+
+adb push treble-overlay-samsung-s21-one-ui.apk /sdcard/
+adb shell su -c mv /sdcard/treble-overlay-samsung-s21-one-ui.apk /data/adb/modules/quickstepswitcher/system/product/overlay/framework-res__auto_generated_rro_product.apk
+adb reboot
